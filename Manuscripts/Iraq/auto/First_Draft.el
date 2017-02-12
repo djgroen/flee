a8a0baefb433e0929da5e124374c6100abcd0bce
BotAnihilator <BotAnihilator@hotmail.com>
2017-02-12 11:52:59 +0100

    Just adding this, although we can do the gross text exchange in the google drive documment, eventually it will be good to have everything in the repository.

---
+++ Manuscripts/Iraq/auto/First_Draft.el
@@ -0,0 +1,52 @@
+(TeX-add-style-hook
+ "First_Draft"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "--shell-escape")
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "fixltx2e"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "lmodern"
+    "mathtools"
+    "url"
+    "color"
+    "amsopn"
+    "nicefrac"
+    "units"
+    "gensymb")
+   (LaTeX-add-labels
+    "sec:orgheadline1"
+    "sec:orgheadline4"
+    "sec:orgheadline2"
+    "sec:orgheadline3"
+    "sec:orgheadline5"
+    "sec:orgheadline6"
+    "sec:orgheadline7"))
+ :latex)
+
