f8d9788bde95aef17fef8bea4d02d83d57a82622
Derek Groen <d.groen@ucl.ac.uk>
2017-10-27 12:05:15 +0100

    Small fixes and debugs on CSV testing.

--- test_csv.py
+++ test_csv.py
@@ -74,6 +74,7 @@ if __name__ == "__main__":
     # Determine number of new refugees to insert into the system.
     new_refs = d.get_daily_difference(t, FullInterpolation=True, ZeroOnDayZero=False) - refugee_debt
     refugees_raw += d.get_daily_difference(t, FullInterpolation=True, ZeroOnDayZero=False)
+
     if new_refs < 0:
       refugee_debt = -new_refs
       new_refs = 0
