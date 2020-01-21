0d93ed0fd8b9d39def1732841f0eddd80dac678b
djgroen <djgroennl@gmail.com>
2020-01-21 15:59:28 +0000

    Added evolve dummy functions.

--- flee/micro_flee.py
+++ flee/micro_flee.py
@@ -6,9 +6,14 @@ import random
 from flee import SimulationSettings
 from flee import flee
 
+
 class Person(flee.Person):
   def __init__(self, location):
     super().__init__(location)
+
+  def evolve(self):
+    super().evolve()
+
 
 class Location(flee.Location):
   def __init__(self, cur_id, name, x=0.0, y=0.0, movechance=0.001, capacity=-1, pop=0, foreign=False, country="unknown"):
@@ -23,6 +28,10 @@ class Link(flee.Link):
 class Ecosystem(flee.Ecosystem):
   def __init__(self):
     super().__init__()
+
+  def evolve(self):
+    super().evolve()
+
 
 if __name__ == "__main__":
   print("No testing functionality here yet.")
