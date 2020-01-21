ed5ea0303d8c9cb83f6e96bd8e6cc8d7784c3538
djgroen <djgroennl@gmail.com>
2020-01-21 15:56:05 +0000

    Added template file for microscale model.

---
+++ flee/micro_flee.py
@@ -0,0 +1,28 @@
+# micro_flee.py
+# sequential microscale model.
+import numpy as np
+import sys
+import random
+from flee import SimulationSettings
+from flee import flee
+
+class Person(flee.Person):
+  def __init__(self, location):
+    super().__init__(location)
+
+class Location(flee.Location):
+  def __init__(self, cur_id, name, x=0.0, y=0.0, movechance=0.001, capacity=-1, pop=0, foreign=False, country="unknown"):
+    super().__init__(name, x, y, movechance, capacity, pop, foreign, country)
+
+
+class Link(flee.Link):
+  def __init__(self, endpoint, distance, forced_redirection=False):
+    super().__init__(endpoint, distance, forced_redirection)
+
+
+class Ecosystem(flee.Ecosystem):
+  def __init__(self):
+    super().__init__()
+
+if __name__ == "__main__":
+  print("No testing functionality here yet.")
