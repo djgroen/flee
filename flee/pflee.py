7bffe5ab23132ab555364db2358a21f530341b35
Hamid Arabnejad <hamid.arabnejad@gmail.com>
2020-07-14 09:48:46 +0100

    remove unused Link class in pflee

--- flee/pflee.py
+++ flee/pflee.py
@@ -116,18 +116,6 @@ class Location(flee.Location):
         self.updateLocationScore()
 
 
-class Link(flee.Link):
-
-    def __init__(self, endpoint, distance, forced_redirection=False):
-        super().__init__(endpoint, distance, forced_redirection)
-
-    def DecrementNumAgents():
-        self.numAgentsOnRank -= 1
-
-    def IncrementNumAgents():
-        self.numAgentsOnRank += 1
-
-
 class Ecosystem(flee.Ecosystem):
 
     def __init__(self):
