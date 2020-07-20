cdb9f963115f7433e33cba360dc962b8735a2b0d
Alireza Jahani <49076559+alirezajahani60@users.noreply.github.com>
2020-07-20 19:00:25 +0100

    Update run_file_coupling.sh

--- ssudan_flee_mscale/run_file_coupling.sh
+++ ssudan_flee_mscale/run_file_coupling.sh
@@ -2,10 +2,10 @@
 
 export PYTHONPATH="$(dirname "$PWD")"
 
-rm -rf out/*
-mkdir -p out/coupled
-mkdir -p out/macro
-mkdir -p out/micro
+rm -rf out/file/*
+mkdir -p out/file/coupled
+mkdir -p out/file/macro
+mkdir -p out/file/micro
 
 python3 ssudan_flee_mscale.py --submodel macro --coupling_type file --end_time 5 2>macro-out &
 python3 ssudan_flee_mscale.py --submodel micro --coupling_type file --end_time 5 2>micro-out &
