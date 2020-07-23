b6d57e1d067af1390d4b7d2a5a6c66c9ca204a63
arabnejad <hamid.arabnejad@gmail.com>
2020-07-23 15:01:24 +0100

    rounds return float variables

--- flee/postprocessing/analysis.py
+++ flee/postprocessing/analysis.py
@@ -1,26 +1,28 @@
 import numpy as np
 
 
+ROUND_NDIGITS = 4
 # Primitive error function for single values.
+
 
 def rel_error(val, correct_val):
     if correct_val < 0.00001:
         return 0.0
-    return np.abs(float(val) / float(correct_val) - 1)
+    return round(np.abs(float(val) / float(correct_val) - 1), ROUND_NDIGITS)
 
 
 def abs_error(val, correct_val):
-    return np.abs(float(val) - float(correct_val))
+    return round(np.abs(float(val) - float(correct_val)), ROUND_NDIGITS)
 
 
 # Primitive error function for arrays.
 
 def abs_diffs(forecast_vals, correct_vals):
-    return np.abs(forecast_vals - correct_vals)
+    return round(np.abs(forecast_vals - correct_vals), ROUND_NDIGITS)
 
 
 def mean_abs_diffs(forecast_vals, correct_vals):
-    return np.mean(np.abs(forecast_vals - correct_vals))
+    return round(np.mean(np.abs(forecast_vals - correct_vals)), ROUND_NDIGITS)
 
 
 def calculate_ln_accuracy_ratio(forecast_vals, actual_vals):
@@ -28,7 +30,7 @@ def calculate_ln_accuracy_ratio(forecast_vals, actual_vals):
     Calculate the log of the accuracy ratio (forecast / actual)
     Return -1 if there is a 0 in the actual values
     """
-    return np.mean(np.abs(np.log(forecast_vals / actual_vals)))
+    return round(np.mean(np.abs(np.log(forecast_vals / actual_vals))), ROUND_NDIGITS)
 
 
 def calculate_MASE(forecast_vals, actual_vals, naieve_vals, start_of_forecast_period=30):
@@ -51,4 +53,4 @@ def calculate_MASE(forecast_vals, actual_vals, naieve_vals, start_of_forecast_pe
     mean_forecast_error = np.sum(
         (np.abs(actual_vals - forecast_vals))) / float(len(actual_vals))
 
-    return mean_forecast_error / mean_naieve_error
+    return round(mean_forecast_error / mean_naieve_error, ROUND_NDIGITS)
