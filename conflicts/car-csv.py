9955c39630aa0d1b5349a1794728cbfd775b423e
Diana Suleimenova <diana.suleimenova@brunel.ac.uk>
2021-01-11 13:49:03 +0000

    Update car-csv.py

--- conflicts/car-csv.py
+++ conflicts/car-csv.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         else:
             end_time = 820
             last_physical_day = 820
-            duration = flee.SimulationSettings.SimulationSettings.ReadFromCSV(sys.argv[
+            duration = flee.SimulationSettings.ReadFromCSV(sys.argv[
                                                                               1])
             if duration > 0:
                 end_time = duration
