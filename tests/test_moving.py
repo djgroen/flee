c31f47407b77399c64294a7197cc780885fbe178
Derek Groen <djgroennl@gmail.com>
2023-08-25 09:33:10 +0200

    Bugfix and added test for #76.

--- tests/test_moving.py
+++ tests/test_moving.py
@@ -1,14 +1,46 @@
 from flee import flee
 from flee.datamanager import handle_refugee_data
 
-"""
-Generation 1 code. Incorporates only distance, travel always takes one day.
-"""
+
+def test_stay_close_to_home():
+    flee.SimulationSettings.ReadFromYML("empty.yml")
+
+    flee.SimulationSettings.move_rules["MaxMoveSpeed"] = 100.0
+    flee.SimulationSettings.move_rules["MaxWalkSpeed"] = 100.0
+    flee.SimulationSettings.move_rules["StayCloseToHome"] = True
+    flee.SimulationSettings.move_rules["HomeDistancePower"] = 5.0
+
+    end_time = 20
+    e = flee.Ecosystem()
+
+    l1 = e.addLocation(name="A", x=0.0, y=0.0, movechance=1.0, foreign=False)
+    l2 = e.addLocation(name="B", x=1.0, y=1.0, movechance=1.0, foreign=False)
+    l3 = e.addLocation(name="C", x=100.0, y=100.0, movechance=1.0, foreign=False)
+
+    e.linkUp(endpoint1="A", endpoint2="B", distance=100.0)
+    e.linkUp(endpoint1="B", endpoint2="C", distance=100.0)
+
+    # Insert refugee agents
+    for _ in range(0, 100):
+        e.addAgent(location=l1, attributes={})
+
+    for t in range(0, end_time):
+        # Propagate the model by one time step.
+        e.evolve()
+
+        print(
+            t,
+            l1.numAgents,
+            l2.numAgents,
+            l3.numAgents,
+        )
+
+    assert t == 19
+    # With such a strong HomeDistancePower, not more than agent should reside in l3.
+    assert l3.numAgents < 2
 
 
 def test_scoring_foreign_weight():
-    print("Testing basic data handling and simulation kernel.")
-
     flee.SimulationSettings.ReadFromYML("empty.yml")
 
     flee.SimulationSettings.move_rules["MaxMoveSpeed"] = 5000.0
