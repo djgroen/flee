3f80f5d552ed36fba712734380ffd5dd9c608679
Derek Groen <djgroennl@gmail.com>
2025-06-11 09:23:00 +0100

    Removed redundant boilerplate in test_twosystem.

--- tests/test_twosystem.py
+++ tests/test_twosystem.py
@@ -2,12 +2,6 @@ import sys
 import os
 import numpy as np
 
-# Import setup (same as before)
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-if parent_dir not in sys.path:
-    sys.path.insert(0, parent_dir)
-
 from flee import flee
 
 
