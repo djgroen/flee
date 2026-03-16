2282d6436b28b5b96e843e60f46a85596fe63709
Derek Groen <djgroennl@gmail.com>
2026-03-16 15:22:20 +0000

    Added KeyError handling.

--- flee/scoring.py
+++ flee/scoring.py
@@ -27,7 +27,11 @@ def updateLocationScore(time: int, loc) -> None:
     if loc.foreign is True:
         score *= SimulationSettings.move_rules["ForeignWeight"] 
     elif SimulationSettings.move_rules["AvoidFoodDeprivedLocations"] and time > 0:
-        score *= 1.0 - (loc.attributes["region_IPC_level"] / 100.0)**1.5
+        try:
+            score *= 1.0 - (loc.attributes["region_IPC_level"] / 100.0)**1.5
+        except KeyError:
+            print(f"ERROR: attribute region_IPC_level missing for location {loc.name} in region {loc.region}.", file=sys.stderr)
+            sys.exit()
    
     #score multiplier for camps
     if loc.camp or loc.idpcamp is True:
